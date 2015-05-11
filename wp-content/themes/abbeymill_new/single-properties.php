e0f924f0d3d4d80b41ca3a26f6fc88f68f5516b5
Tom Hanson <tomhanson1985@gmail.com>
2015-05-11 21:00:09 +0100

    updated single properties for modal

--- wp-content/themes/abbeymill_new/single-properties.php
+++ wp-content/themes/abbeymill_new/single-properties.php
@@ -1,37 +1,26 @@
+
 <?php get_header('individual'); ?>
 <div style="box-shadow: 0 5px 9px rgba(0, 0, 0, 0.18); overflow: hidden;">
     <div class="single-property-tabs">
         <div class="tab-wrapper">            
-
             <div id="panel1" class="panel">
                 <div class="container">
                     <div class="row">
                         <div class="col-xs-12">
                             <div class="flexslider">
                                 <ul class="slides">
-
                                     <?php $images = get_field('gallery'); ?>    
-                                    
                                     <?php if($images) { ?>
-
                                         <?php foreach( $images as $image ): ?>
-
                                                 <li><img src="<?php echo $image['sizes']['slider']; ?>" alt="<?php echo $image['alt']; ?>" /></li>           
-
                                         <?php endforeach; ?> 
-                                    
                                     <?php } ?>
-
                                 </ul>
                             </div>
                         </div>
                     </div>
                 </div>
-
-
-
             </div>
-
             <div id="panel2" class="panel">
                 <div class="container">
                     <div class="row">
@@ -49,31 +38,6 @@
                     </div>
                 </div>
             </div>
-            <?php $brochure = get_field('brochure'); ?>
-            <?php if($brochure) { ?>
-            <div id="panel3" class="panel">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12">
-                            
-                            <div id="flipbook-wrapper">
-                                <div id="flipbook" style="margin: auto;">
-                                    <?php foreach( $brochure as $page ): ?>
-                                    <div class="wowbook-hardpage">
-                                        <img src="<?php echo $page['sizes']['large']; ?>" alt="<?php echo $page['alt']; ?>" />
-                                    </div>
-                                    <?php endforeach; ?> 
-                                </div> 
-
-                                <div class="download-button">
-                                <a href="<?php the_field('brochure_download'); ?>" target="_blank" class="btn">Download Brochure</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <?php } ?>
 
             <div id="panel4" class="panel" style="height:350px;">
                 <?php   
@@ -82,8 +46,6 @@
                 ?>
                 <div id="single-map" style="width:auto !important;height:350px !important;display:block;"></div>
             </div>
-
-
             <script src="//maps.google.com/maps/api/js?sensor=true"></script>
             <script src="<?php print TEMPPATH; ?>/js/gmaps.js"></script>
             <script>
@@ -103,14 +65,11 @@
                 google.maps.event.addDomListener(window, 'load', initialize);
             </script>
         </div> 
-
     </div>
 </div>
-        
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-
                 <div id="content">
                     <div class="property-title">
                         <h2><?php the_title(); ?></h2>
@@ -122,11 +81,8 @@
                                 <li><a href="#panel1">Overview</a></li>
                                 <li><a href="#panel2">Specification</a></li>
                                 <?php if( get_field('brochure') ) { ?>
-
-                                <li><a id="brochure" href="#panel3">Brochure</a></li>
-
-                                <?php } ?> 
-
+                                <li id="brochure" data-target="#panel3" data-toggle="modal">Brochure</li>
+                                <?php } ?>
                                 <li><a id="location" href="#panel4">Location</a></li>
                             </ul> 
                         </div>  
@@ -138,7 +94,6 @@
             <div class="col-sm-8 col-md-8">
                 <h4><?php the_field('property_address'); ?></h4>
                 <p><?php the_field('property_description'); ?></p>
-
                 <h5>For further information please contact
                 <?php bloginfo( 'name' ); ?> – 01234 714844.</h5>
                 <div class="information-button">
@@ -148,10 +103,41 @@
             <div class="col-sm-4 col-md-3 col-md-offset-1">
                 <h3>Price: £<span><?php the_field('property_price'); ?></span></h3>
                 <h3>Completion: <span><?php the_field('completion_date'); ?></span></h3>
-
-            </div>           
-
+            </div>
         </div>
 </div>
+<?php $brochure = get_field('brochure'); ?>
+<?php if($brochure) { ?>
+
+<!-- Modal -->
+<div class="modal fade" id="panel3" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+  <div class="pane modal-dialog">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    </div>
+     <div class="modal-content">
+      <div class="container-fluid modal-body">
+          <div class="row">
+              <div class="col-xs-12">
+                  <div id="flipbook-wrapper" style="width: 100%;">
+                      <div id="flipbook" style="margin: auto; max-width: 100%;">
+                          <?php foreach( $brochure as $page ): ?>
+                          <div class="wowbook-hardpage">
+                              <img src="<?php echo $page['sizes']['large']; ?>" alt="<?php echo $page['alt']; ?>" />
+                          </div>
+                          <?php endforeach; ?>
+                      </div>
+                      <div class="download-button">
+                        <p>Click the edges of the book to turn the page or download a brochure by clicking below</p>
+                        <a href="<?php the_field('brochure_download'); ?>" target="_blank" class="btn">Download Brochure</a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php } ?>
 
 <?php get_footer('individual'); ?>
